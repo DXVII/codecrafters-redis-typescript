d08a97839f27e5984aadbebcf0e35ae649cfa662
DXVII <david.pham.d@gmail.com>
2024-07-25 22:47:04 +1000

    test server locally on localhost:6379

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,15 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+      connection.on('data', (data) => console.log(`Received: ${data}`));
+
+    connection.write('HTTP/1.1 200 OK\r\n');
+    connection.write('Content-Type: text/html; charset=UTF-8\r\n\r\n');
+    connection.write('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Hello from TypeScript connection!</h1></body></html>');
+    // connection.end();
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(6379, "127.0.0.1", () => {
+  console.log("Server is listening on 127.0.0.1:6379");
+});
