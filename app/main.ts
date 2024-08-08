e107c92b7052448ef2be2324821d9486cc273884
DXVII <david.pham.d@gmail.com>
2024-08-08 15:43:51 +1000

    stage 2: ping --> pong

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,11 @@ import * as net from "net";
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
       connection.on('data', (data) => console.log(`Received: ${data}`));
-
-    connection.write('HTTP/1.1 200 OK\r\n');
-    connection.write('Content-Type: text/html; charset=UTF-8\r\n\r\n');
-    connection.write('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Test page display!</h1></body></html>');
-    // connection.end();
+      connection.write("+PONG\r\n")
+      connection.end();
+    // connection.write('HTTP/1.1 200 OK\r\n');
+    // connection.write('Content-Type: text/html; charset=UTF-8\r\n\r\n');
+    // connection.write('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Test page display!</h1></body></html>');
 });
 
 server.listen(6379, "127.0.0.1", () => {
